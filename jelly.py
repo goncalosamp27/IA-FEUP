5afbbeecb9d437c2bf06b409d4bb6e15e9abbc28
goncalosamp27 <goncalo20.sampaio03@gmail.com>
2025-03-10 12:22:24 +0000

    jelly piece drawer

--- jelly.py
+++ jelly.py
@@ -1,9 +1,9 @@
 import random
+import pygame
 
 class Jelly:
-
-    COLORS = ['#be2528', '#2536be', '#7525be', '#3eb34b', '#64bfbe', '#e2d614']
-    # red, blue, purple, green, cyan, yellow
+    COLORS = ['#be2528', '#2536be', '#7525be', '#3eb34b', '#64bfbe', '#e2d614'] # red, blue, purple, green, cyan, yellow
+    SIZE = 40
 
     def __init__(self, posX, posY, tl, tr, bl, br):
         self.posX = posX
@@ -35,5 +35,18 @@ class Jelly:
     def get_colors(self):
         return self.tl, self.tr, self.bl, self.br
     
-    def get_colors_in_position(self, posX, posY):
-        return self.tl, self.tr, self.bl, self.br
+    def draw(self, screen):
+        size = self.SIZE
+        x, y = self.posX, self.posY
+
+        # Draw the four colored quadrants
+        pygame.draw.polygon(screen, self.tl, [(x, y), (x + size // 2, y), (x, y + size // 2)]) # Top-left
+        pygame.draw.polygon(screen, self.tr, [(x + size, y), (x + size // 2, y), (x + size, y + size // 2)]) # Top-right
+        pygame.draw.polygon(screen, self.bl, [(x, y + size), (x + size // 2, y + size), (x, y + size // 2)]) # Bottom-left
+        pygame.draw.polygon(screen, self.br, [(x + size, y + size), (x + size // 2, y + size), (x + size, y + size // 2)]) # Bottom-right
+
+        # Smooth corners using quarter circles
+        pygame.draw.circle(screen, self.tl, (x + size // 4, y + size // 4), size // 4)
+        pygame.draw.circle(screen, self.tr, (x + size - size // 4, y + size // 4), size // 4)
+        pygame.draw.circle(screen, self.bl, (x + size // 4, y + size - size // 4), size // 4)
+        pygame.draw.circle(screen, self.br, (x + size - size // 4, y + size - size // 4), size // 4)
