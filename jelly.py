748fe19ea531152c36320397816739337533de1a
goncalosamp27 <goncalo20.sampaio03@gmail.com>
2025-03-10 11:40:22 +0000

    added class for jelly piece

---
+++ jelly.py
@@ -0,0 +1,39 @@
+import random
+
+class Jelly:
+
+    COLORS = ['#be2528', '#2536be', '#7525be', '#3eb34b', '#64bfbe', '#e2d614']
+    # red, blue, purple, green, cyan, yellow
+
+    def __init__(self, posX, posY, tl, tr, bl, br):
+        self.posX = posX
+        self.posY = posY
+        self.tl = tl # top left
+        self.tr = tr # top right
+        self.bl = bl # bottom left
+        self.br = br # bottom right
+
+    def get_position(self):
+        return self.posX, self.posY 
+
+    def set_position(self, posX, posY):
+        self.posX = posX
+        self.posY = posY
+
+    def set_corner_color(self, tl, tr, bl, br):
+        self.tl = tl
+        self.tr = tr
+        self.bl = bl
+        self.br = br
+        
+    def set_random_colors(self):
+        self.tl = random.choice(self.COLORS)
+        self.tr = random.choice(self.COLORS)
+        self.bl = random.choice(self.COLORS)
+        self.br = random.choice(self.COLORS)
+
+    def get_colors(self):
+        return self.tl, self.tr, self.bl, self.br
+    
+    def get_colors_in_position(self, posX, posY):
+        return self.tl, self.tr, self.bl, self.br
--- main.py
+++ main.py
@@ -23,4 +23,5 @@ def main():
         
 
 
+        
 main()
