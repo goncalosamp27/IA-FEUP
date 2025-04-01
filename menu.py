96c883c6e5056c2c43b289a3905ab2c9160e94f0
tatianawl <tatiana.lin.wang@gmail.com>
2025-04-01 20:25:09 +0100

    volume on off button but not working perfectly

--- menu.py
+++ menu.py
@@ -8,6 +8,15 @@ pygame.display.set_caption("Jelly Field")
 
 BG = pygame.image.load("assets/Background7.png")
 
+#MUTE = pygame.image.load("assets/images/unmute.svg")
+#UNMUTE = pygame.image.load("assets/images/mute.svg")
+
+MUTE = pygame.image.load("assets/images/unmute2.png")
+UNMUTE = pygame.image.load("assets/images/mute2.png")
+
+# track sound status (on/off)
+sound_on = True
+
 #Background Music
 pygame.mixer.music.load("assets/music/background1.mp3")  
 pygame.mixer.music.set_volume(0.5)  
@@ -17,6 +26,36 @@ pygame.mixer.music.play(-1)
 CLICK_SOUND = pygame.mixer.Sound("assets/music/1.mp3")
 CLICK_SOUND.set_volume(0.7) 
 
+
+def toggle_sound():
+    global sound_on
+    if sound_on:
+        pygame.mixer.music.pause()  # Pause music
+        print("Sound Paused")  # Debug message
+    else:
+        pygame.mixer.music.unpause()  # Unpause music
+        print("Sound Unpaused")  # Debug message
+    sound_on = not sound_on  # Toggle sound state
+
+
+def draw_microphone_icon():
+    icon_width, icon_height = 50, 50
+    icon_x, icon_y = 10, SCREEN.get_height() - icon_height - 10  # bottom-left corner
+
+    resized_unmute = pygame.transform.scale(UNMUTE, (icon_width, icon_height))
+    resized_mute = pygame.transform.scale(MUTE, (icon_width, icon_height))
+
+    if sound_on:
+        SCREEN.blit(resized_unmute, (icon_x, icon_y))
+    else:
+        SCREEN.blit(resized_mute, (icon_x, icon_y))
+
+    mouse_x, mouse_y = pygame.mouse.get_pos()
+    if pygame.mouse.get_pressed()[0]: 
+        if icon_x <= mouse_x <= icon_x + icon_width and icon_y <= mouse_y <= icon_y + icon_height:
+            toggle_sound() 
+        
+   
 def get_font(size):
     return pygame.font.Font("assets/font.ttf", size)
 
@@ -322,6 +361,8 @@ def main_menu():
                             text_input="EXIT", font=get_font(60), base_color="#99afd7", hovering_color="White")
 
         SCREEN.blit(MENU_TEXT, MENU_RECT)
+        
+        draw_microphone_icon()
 
         #for button in [PLAY_BUTTON, OPTIONS_BUTTON, HTP_BUTTON, QUIT_BUTTON]:
         #    button.changeColor(MENU_MOUSE_POS)
